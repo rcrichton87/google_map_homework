e8fe224b3911fa178653b0f6869939a78462e15e
Ross Crichton <rcrichton87@googlemail.com>
2017-04-11 18:35:33 +0100

    where am I button added

--- public/app.js
+++ public/app.js
@@ -5,6 +5,8 @@ var initialize = function(){
 
   var tokyoButton = document.querySelector('#tokyoButton');
 
+  var whereAmIButton = document.querySelector('#whereAmIButton')
+
   mainMap.addMarker(center, '<h1>CodeClan</h1> <p>37 Castle Terrace, Edinburgh EH1 2EL</p> <a href="https://codeclan.com/">Website,</a>');
   
   mainMap.addClickEvent(); 
@@ -13,7 +15,14 @@ var initialize = function(){
     mainMap.googleMap.setCenter({lat: 35.696201, lng: 139.729614})
   }
 
+  var handleWhereAmIButtonClicked = function(){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      mainMap.googleMap.setCenter({lat: position.coords.latitude, lng: position.coords.longitude});
+    });
+  }
+
   tokyoButton.onclick = handleTokyoButtonClick;
+  whereAmIButton.onclick = handleWhereAmIButtonClicked;
 
 }
 
